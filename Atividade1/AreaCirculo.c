e4745adb963cc35c17f00d81275961892f5d3e6e
Kelvin963 <sousaks054@gmail.com>
2023-09-05 13:07:24 -0300

    Area de um circulo

--- Atividade1/AreaCirculo.c
+++ Atividade1/AreaCirculo.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main (){
+    int main (){
+
+double A ;
+double pi ;
+int r ;
+
+pi = 3.14159 ;
+
+printf ("pi e igual a %f\n", pi);
+
+printf ("Digite o raio(r) do circulo:");
+scanf ("%d", &r);
+
+r = r*r;
+
+printf ("Se o raio ao quadrado e igual a %d\n", r);
+
+A = pi * r;
+
+printf ("Entao a area do circulo e igual a %f", A );
+
+
 
 
 
