9a8a833ad687bb3b2f59d34294e02c96c72202d1
Kelvin963 <sousaks054@gmail.com>
2023-09-28 08:40:02 -0300

    ok

--- A1-Exemplos/omaior.c
+++ A1-Exemplos/omaior.c
@@ -5,17 +5,24 @@ int main(){
 int a;
 int b;
 int c;
-int maior;
-
-scanf ("%d %d %d", &a, &b, &c); //pro scanf, espaço ou enter ("\n") é a mesma coisa
-
-maior = (a + b + abs(a - b))/2;  //maior entre a e b
-maior = (maior + c + abs (maior - c))/2;
-
-printf("%d é o maior", maior);
+//int maior;
 
 
+printf ("Insira tres numeros para saber o maior: \n");
+scanf("%d %d %d", &a, &b, &c);
 
+if(a > b && a > c){
+
+    printf("%d eh o maior", a);
+
+}else if(b > a && b > c){
+    printf("%d eh o maior", b);
+
+
+}else if(c > b && c > a){
+    printf("%d eh o maior", c);
+
+}
 
 
 
@@ -23,3 +30,19 @@ printf("%d é o maior", maior);
 
 
 }
+
+/*scanf ("%d %d %d", &a, &b, &c); //pro scanf, espaço ou enter ("\n") é a mesma coisa
+
+maior = (a + b + abs(a - b))/2;  //maior entre a e b
+maior = (maior + c + abs (maior - c))/2;
+
+printf("%d eh o maior", maior);
+
+
+
+*/
+
+
+
+
+
