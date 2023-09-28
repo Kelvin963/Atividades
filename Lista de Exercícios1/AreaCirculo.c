bc5bf47d8c0b31d26a5fa5fca1c39145637d57b3
Kelvin963 <sousaks054@gmail.com>
2023-09-28 10:43:24 -0300

    a

--- Lista de Exercícios1/AreaCirculo.c
+++ Lista de Exercícios1/AreaCirculo.c
@@ -8,20 +8,16 @@ int r ;
 
 pi = 3.14159 ;
 
-printf ("pi e igual a %f\n", pi);
 
-printf ("Digite o raio(r) do circulo:");
 scanf ("%d", &r);
 
 r = r*r;
 
-printf ("Se o raio ao quadrado e igual a %d\n", r);
-
 A = pi * r;
 
-printf ("Entao a area do circulo e igual a %f", A );
+printf ("A=%.4lf\n", A );
 
 
 
-    
+return 0;
 }
---
+++ Lista de Exercícios1/PROD.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+int valor1;
+int valor2;
+int PROD;
+
+scanf("%d %d", &valor1, &valor2);
+PROD = valor1 * valor2;
+printf("PROD = %d\n", PROD);
+
+return 0;
+}
